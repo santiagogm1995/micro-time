12937140092c8636692bad2c114f746e8f937090
Santiago González <santiagogonzalezmota@gmail.com>
2021-12-07 20:37:05 +0100

    fix: Fixed test

--- src/test/java/com/santiago/micro/controller/TimeControllerIntegrationTest.java
+++ src/test/java/com/santiago/micro/controller/TimeControllerIntegrationTest.java
@@ -2,8 +2,6 @@ package com.santiago.micro.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import javax.ws.rs.core.MediaType;
-
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +11,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@TestPropertySource(properties = { "xsuaa.uaadomain=localhost"})
+@TestPropertySource(properties = { "xsuaa.uaadomain=localhost" })
 public class TimeControllerIntegrationTest {
 
 	@Autowired
 	private MockMvc mvc;
-	
+
 	@Value("${xsuaa.urlToken}")
 	private String urlToken;
 
@@ -33,21 +33,26 @@ public class TimeControllerIntegrationTest {
 
 	@Before
 	private String obtainAccessToken() throws Exception {
-		
-		HttpResponse<String> response = Unirest.post(urlToken + "/oauth/token?grant_type=client_credentials")
-		  .header("Authorization", "Basic " + xsuaaBasicAuth)
-		  .asString();
-		
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		MediaType mediaType = MediaType.parse("text/plain");
+		RequestBody body = RequestBody.create("", mediaType);
+		Request request = new Request.Builder().url(urlToken + "/oauth/token?grant_type=client_credentials")
+				.method("POST", body)
+				.addHeader("Authorization", "Basic " + xsuaaBasicAuth)
+				.addHeader("Content-Type", "application/json").build();
+
+		Response response = client.newCall(request).execute();
+
 		JacksonJsonParser jsonParser = new JacksonJsonParser();
-        return jsonParser.parseMap(response.getBody()).get("access_token").toString();
+		return jsonParser.parseMap(response.body().string()).get("access_token").toString();
 	}
 
 	@Test
 	public void getWeatherForMadrid() throws Exception {
 		final String accessToken = obtainAccessToken();
 		mvc.perform(get("/time/madrid").header("Authorization", "Bearer " + accessToken)
-				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+				.contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(javax.ws.rs.core.MediaType.APPLICATION_JSON));
 
 	}
 
